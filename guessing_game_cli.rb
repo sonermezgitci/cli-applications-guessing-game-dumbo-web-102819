506d4d3b53c58f839f71e9ae5d018867e7d6d5b7
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-27 23:02:02 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,9 +2,9 @@ def run_guessing_game
   puts "//Get a number between 1 to 6"
   roll=1+rand(6)
   if user_input = roll 
-    puts "You gueessed the correct number!"
+    puts "\nYou gueessed the correct number!"
   else 
-    puts "Sorry! The computer guessed correct number".
+    puts "Sorry! The computer guessed correct number!\n".
     elsif user_input = exit
     puts "Goodbye"
   end 
