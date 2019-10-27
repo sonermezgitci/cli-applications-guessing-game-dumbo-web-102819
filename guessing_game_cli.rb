c9809721f4ac0d777a3320ecfde80cfe91e74322
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-27 22:56:55 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,4 +2,11 @@ def run_guessing_game
   puts // "Get a number between 1 and 6"
   roll=1+rand(6)
   if user_input = roll 
-    if 
+    puts "You gueessed the correct number"!
+  else 
+    puts "Sorry! The computer guessed <number>".
+    elsif user_input = "exit"
+    puts "Goodbye"
+  end 
+end 
+end
